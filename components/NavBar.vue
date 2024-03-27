bda4ac23bebb740251e39e2f8649a1f4783a55a9
uwla <uwlalinux@gmail.com>
2024-03-27 10:49:35 -0300

    feat: add navbar

---
+++ components/NavBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav class="flex justify-between px-12 py-5" style="background: rgba(33, 36, 53, 0.9); color: #fff">
+    <div class="flex items-center gap-4">
+      <img src="/img/logo.png" alt="VueSchool logo">
+      <a href="" class="w-min text-lg font-bold">
+        Vue School.io
+      </a>
+    </div>
+    <div class="flex gap-5 items-center">
+      <div>Courses</div>
+      <a>Plans</a>
+      <div>Business service</div>
+      <a>Learning Paths</a>
+      <a>Hiring</a>
+      <a>Articles</a>
+      <div>Events</div>
+      <a>Get Certified</a>
+    </div>
+    <div class="flex gap-7">
+      <button>Login</button>
+      <div style="padding: 2px; width:fit-content; height: fit-content; border-radius: 10px;  background: linear-gradient(95.39deg, #0BD88F 0%, #00C2FF 100%);">
+        <button class="py-3 px-7 green weight-600 font-semibold"
+        style="background: rgba(33, 36, 53, 0.9); border-radius: 10px ">
+          Sign up
+        </button>
+      </div>
+    </div>
+  </nav>
+
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/tailwindcss"],
+  css: [
+    "~/assets/css/app.css",
+    "@fontsource/rubik/400.css",
+    "@fontsource/rubik/500.css",
+    "@fontsource/rubik/600.css",
+    "@fontsource/rubik/700.css",
+    "@fontsource/rubik/800.css",
+  ],
+});
